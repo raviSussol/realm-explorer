943213841cdbc4a2b7876d918a819a841f299e8a
raviSussol <ravi.shrestha@sussol.net>
2018-07-01 16:15:34 +0545

    Updated home screen text

--- App.js
+++ App.js
@@ -12,26 +12,16 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <Text style={styles.greet}>
+          Hello There...!
+        </Text>
         <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+          Welcome to Realm Explorer!
         </Text>
       </View>
     );
@@ -46,13 +36,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#F5FCFF',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 25,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
+  greet: {
+    fontSize: 40,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    fontWeight: 'bold',
   },
 });
